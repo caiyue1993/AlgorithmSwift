b0883781f514d0caf6810be73bcbd1069b104f58
Yue Cai <yuecai.nju@gmail.com>
2020-04-22 23:34:08 +0800

    feat: add permuteUnique backtrack

--- Backtrace/Sources/Backtrace/Backtrace.swift
+++ Backtrace/Sources/Backtrace/Backtrace.swift
@@ -2,7 +2,6 @@
 // 全排列的例子
 
 struct Backtrace {
-    
     func permute(_ nums: [Int]) -> [[Int]] {
         
         var results: [[Int]] = []
@@ -14,7 +13,7 @@ struct Backtrace {
         ///   - nums: 全排列的初数组
         ///   - track: 当前的路径
         func backtrace(_ nums: [Int], _ track: inout [Int]) {
-            // 触发结束条件
+            /// 触发结束条件
             if track.count == nums.count {
                 results.append(track)
                 return
@@ -38,4 +37,46 @@ struct Backtrace {
         return results
     }
     
+    /// Given: [1, 1, 2] which means could duplicate
+    /// Output: [1, 1, 2], [1, 2, 1], [2, 1, 1]
+    func permuteUnique(_ nums: [Int]) -> [[Int]] {
+        var results: [[Int]] = []
+        var track: [Int] = []
+        var isVisited: [Bool] = Array(repeating: false, count: nums.count)
+           
+        let sortedNums = nums.sorted() // 需要先对数组进行排序
+           
+        func backtrack(nums: [Int], _ track: inout [Int], _ isVisited: inout [Bool]) {
+            if track.count == nums.count {
+                results.append(track)
+                return
+            }
+           
+            var i = 0
+            while (i < nums.count) {
+                if isVisited[i] {
+                    // 如果之前的已经访问了，那么不再使用，与 permute 相比，permute 是通过 contains 来判断是否已访问的
+                    // 所以说 permute 的 contains 优化程度不够，也可以通过这种 isVisited 这种方式来用空间换时间
+                    i = i + 1
+                    continue
+                }
+                if (i > 0 && !isVisited[i-1] && nums[i-1] == nums[i]) {
+                    // 由于我们的 nums 是排过序的，所以如果当前节点和 sibling 一样
+                    // 并且一定得是 !isVisited[i-1]，表示是 sibling（由于是排序的所以 i-1 用来表示上一个 sibling，如果是已经 visit 过的，那么再加入进来是应该的
+                    i = i + 1
+                    continue
+                }
+                track.append(nums[i])
+                isVisited[i] = true
+                backtrack(nums: nums, &track, &isVisited)
+                track.removeLast()
+                isVisited[i] = false
+                
+                i = i + 1
+           }
+       }
+           
+        backtrack(nums: sortedNums, &track, &isVisited)
+        return results
+    }
 }
--- Backtrace/Tests/BacktraceTests/BacktraceTests.swift
+++ Backtrace/Tests/BacktraceTests/BacktraceTests.swift
@@ -2,13 +2,20 @@ import XCTest
 @testable import Backtrace
 
 final class BacktraceTests: XCTestCase {
-    func testExample() {
+    func testPermute() {
         let nums = [1,2,3]
-        let expectedResults = [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
-        XCTAssertEqual(Backtrace().permute(nums), expectedResults)
+        let expectedResults = [[1,3,2], [1,2,3],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+        XCTAssertEqual(Set(Backtrace().permute(nums)), Set(expectedResults))
     }
 
+    func testPermuteUnique() {
+        let nums = [1, 1, 1, 2]
+        let expectedResults = [[1, 1, 1, 2], [1, 1, 2, 1], [1, 2, 1, 1], [2, 1, 1, 1]]
+        XCTAssertEqual(Set(Backtrace().permuteUnique(nums)), Set(expectedResults))
+    }
+    
     static var allTests = [
-        ("testExample", testExample),
+        ("testPermute", testPermute),
+        ("testPermuteUnique", testPermuteUnique)
     ]
 }
